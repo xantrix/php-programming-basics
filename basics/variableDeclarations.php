2f8a20818d02221da2823e551c315a90f60e19f7
XanTriX <x4ntrix@gmail.com>
2014-01-24 12:58:16 +0100

    Change-Id: I2a757206bfeb81d1eb36163f9f8a499f5b4341d9

---
+++ basics/variableDeclarations.php
@@ -0,0 +1,10 @@
+<?php
+$simple = 'hi';
+$_underscore = 'hi';
+$ùàèì = 'hi';
+@$foo = 'hi';
+//&$variable = new stdClass(); //syntax error, unexpected '&'
+${00} = "hi";
+//$00 = "hi"; //invalid because we can\'t start variable with numbers
+
+echo "see in debug";
